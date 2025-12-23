220196b37574745f7b3195aba5facca6c5522801
Riyuji <riyujin.danma@gmail.com>
2025-12-23 09:50:48 +0100

    removing netrw banner and changing winsize

--- lua/primeryujin/settings.lua
+++ lua/primeryujin/settings.lua
@@ -1,7 +1,16 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+vim.g.have_nerd_font = true
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+-- Mouse for resplit size
+vim.opt.mouse = "a"
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -17,12 +26,15 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.opt.hlsearch = false
+
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 15
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
@@ -30,4 +42,8 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
 -- vim.opt.winborder = "rounded"
+vim.opt.inccommand = "split"
