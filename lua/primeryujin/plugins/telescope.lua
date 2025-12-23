a144f6472f76f5e10a5829726e479a79782a1410
Riyuji <riyujin.danma@gmail.com>
2025-12-23 09:59:51 +0100

    Todo: Trying to make telescope transparant exept borders

--- lua/primeryujin/plugins/telescope.lua
+++ lua/primeryujin/plugins/telescope.lua
@@ -5,8 +5,13 @@ return {
 		"nvim-telescope/telescope-fzf-native.nvim", -- optional C sorter for speed
 	},
 	build = "make", -- compiles the native C extension
-	config = function()
+	config = function(_, opts)
 		-- Load FZF extension if installed
+
+		opts.defaults = opts.defaults or {}
+		opts.defaults.file_ignore_patterns = opts.defaults.file_ignore_patterns or {}
+		opts.defaults.winblend = 15
+
 		local telescope = require("telescope")
 		pcall(telescope.load_extension, "fzf")
 	end,
